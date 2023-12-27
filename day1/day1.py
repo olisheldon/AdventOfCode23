dc64c5e28c3b60a1d71bf95f93df0aeb55ad1939
Oliver Sheldon <oliversheldon00@gmail.com>
2023-12-27 17:04:58 +0000

    Day1 part 2 python implementation finished

--- day1/day1.py
+++ day1/day1.py
@@ -1,5 +1,6 @@
-from collections import OrderedDict, defaultdict
 import string
+import sys
+
 
 class Day1:
 
@@ -13,6 +14,8 @@ class Day1:
                        "eight" : 8,
                        "nine"  : 9,
                        }
+    min_char_string_length = min([len(key) for key in char_string_map])
+    max_char_string_length = max([len(key) for key in char_string_map])
 
     inverse_char_string_map = {k[::-1] : v for k, v in char_string_map.items()}
 
@@ -20,83 +23,59 @@ class Day1:
         self.input = Day1.get_input()
 
     def part_1(self) -> int:
-        return Day1.sum_outer_numbers(self.input)
+        lines = Day1.preprocess_strings(self.input, Day1.string_preprocessing_part_1)
+        return Day1.handle_boundary_integers(lines)
     
     def part_2(self) -> int:
-        pass
+        lines = Day1.preprocess_strings(self.input, Day1.string_preprocessing_part_2)
+        return Day1.handle_boundary_integers(lines)
 
     @staticmethod
-    def sum_outer_numbers(lines: list[str]) -> int:
+    def handle_boundary_integers(lines: list[list[int]]) -> int:
+        assert len(lines) >= 1 and all(all((type(item) == int) for item in line) for line in lines)
         value = 0
         for line in lines:
-            for c in line:
-                if c.isdigit():
-                    value += 10 * int(c)
-                    break
-            for c in line[::-1]:
-                if c.isdigit():
-                    value += int(c)
-                    break
+            value += 10 * line[0]
+            value += line[-1]
         return value
     
     @staticmethod
-    def preprocess_strings(lines: list[str]) -> list[int]:
+    def preprocess_strings(lines: list[str], method) -> list[list[int]]:
         new_lines = []
         for line in lines:
-            new_lines.append(Day1.string_preprocessing(line))
+            new_lines.append(method(line))
         return new_lines
     
     @staticmethod
-    def string_preprocessing(line: str) -> int:
-        min_char_string_length = min([len(key) for key in Day1.char_string_map])
-        max_char_string_length = max([len(key) for key in Day1.char_string_map])
+    def string_preprocessing_part_1(line: str) -> list[list[int]]:
         new_line = []
-        l, r = 0, min_char_string_length
+        for c in line:
+            if c.isdigit():
+                new_line.append(int(c))
+        return new_line
+    
+    @staticmethod
+    def string_preprocessing_part_2(line: str) -> list[int]:
+        new_line = []
+        l = 0
         while l < len(line):
             if line[l] in string.digits:
                 new_line.append(int(line[l]))
             else:
-                for number in Day1.char_string_map:
-                    r = l + min_char_string_length
-                    while r - l <= max_char_string_length:
-                        if r > len(line):
-                            r = len(line)
-                        print(l, r, line[l:r], number)
-                        if number == line[l:r]:
-                            new_line.append(Day1.char_string_map[number])
-                            l = r
-                            r = l + min_char_string_length
-                            break
-                        r += 1
+                answer = Day1.isStringRangeValid(line, l)
+                if answer:
+                    new_line.append(answer)
             l += 1
-            r = l + min_char_string_length
         return new_line
-
-        # for line in lines:
-        #     digit_pos = 1
-        #     while digit_pos:
-        #         digit_strings = defaultdict(list)
-        #         for digit in Day1.char_string_map:
-        #             digit_pos = line.find(digit)
-        #             if digit_pos:
-        #                 digit_strings[digit_pos].append(digit)
-        #         if digit_strings:
-        #             for i in len(line):
-        #                 if i in digit_strings:
-        #                     line = line[:i] + str(digit_strings) + line[i + len(Day1.char_string_map[digit_strings]):]
-
-            
-                    
-
-
-                
-                
-
-
+    
     @staticmethod
-    def sum_outer_strings(lines: list[str]) -> int:
-        pass
-
+    def isStringRangeValid(line, l):
+        r = l + Day1.min_char_string_length
+        while r - l <= Day1.max_char_string_length and r < len(line) + 1:
+            if line[l:r] in Day1.char_string_map:
+                return Day1.char_string_map[line[l:r]]
+            r += 1
+        return 0
 
     @staticmethod
     def get_input() -> list[str]:
@@ -104,8 +83,16 @@ class Day1:
             lines = f.read().splitlines()
         return lines
 
-if __name__ == '__main__':
-    day1 = Day1()
-    print(day1.string_preprocessing("9sixsevenz3"))
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        raise RuntimeError(f"Argument is not recognised. Please enter part1 or part2.")
 
+
+    day1 = Day1()
+    if sys.argv[1].lower() == "part1":
+        print(day1.part_1())
+    elif sys.argv[1].lower() == "part2":
+        print(day1.part_2())
+    else:
+        raise RuntimeError(f"Argument {sys.argv[1]} is not recognised. Please enter part1 or part2.")
     
