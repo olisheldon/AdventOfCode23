ad22e0657d606eac20cff8a666b000a9f890cd26
Oliver Sheldon <oliversheldon00@gmail.com>
2023-12-27 17:39:08 +0000

    Refactored Day1 cpp part 1 solution to use class. This will make way for part2 solution

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -6,7 +6,11 @@
 #include <set>
 #include <cctype>
 
-int sumOuterStrings(std::vector<std::string> input) {
+class Day1 {
+
+
+public:
+    std::vector<std::string> lines;
     std::unordered_map<std::string, int> wordsToInt{{"one",   1},
                                                     {"two",   2},
                                                     {"three", 3},
@@ -16,56 +20,67 @@ int sumOuterStrings(std::vector<std::string> input) {
                                                     {"seven", 7},
                                                     {"eight", 8},
                                                     {"nine",  9}};
-    std::vector<std::string> words;
-    for (const auto& pair : wordsToInt) {
-        words.push_back(pair.first);
+    std::vector<std::string> words = {"one",
+                                      "two",
+                                      "three",
+                                      "four",
+                                      "five",
+                                      "six",
+                                      "seven",
+                                      "eight",
+                                      "nine"};
+
+    Day1() {
+        lines = getLines();
     }
 
-    int sum{};
-    std::vector<int> integers;
-    for (const auto& line : input) {
-        for (const auto& word : words) {
-            int pos{};
-            while (pos < line.size()){
-                pos = line.find(word);
-                if (pos != std::string::npos) {
-                    integers.push_back(wordsToInt[word]);
+    std::vector<std::string> getLines() {
+        std::ifstream myfile("input.txt");
+        std::string line;
+        std::vector<std::string> input;
+        while (std::getline(myfile, line))
+        {
+            input.push_back(line);
+        }
+        return input;
+    }
+
+    int part1() {
+        std::vector<std::vector<int>> processedLines = preprocessStrings();
+        return sumOuterNumbers(processedLines);
+    }
+
+    std::vector<std::vector<int>> preprocessStrings() {
+        std::vector<std::vector<int>> processedLines{};
+        std::vector<int> processedLine{};
+        for (const auto& line : lines) {
+            processedLine = {};
+            for (const auto& c : line) {
+                if (std::isdigit(c)) {
+                    processedLine.push_back(static_cast<int>(c - '0'));
                 }
             }
+            processedLines.push_back(processedLine);
         }
+        return processedLines;
     }
-    return sum;
-}
 
-int sumOuterNumbers(std::vector<std::string> input) {
-    int sum{};
-    for (const auto& line : input) {
-        for (const char& c : line) {
-            if (std::isdigit(c)) {
-                sum += 10 * static_cast<int>(c - '0');
-                break;
-            }
+    int sumOuterNumbers(std::vector<std::vector<int>> processedLines) {
+        int sum{};
+        for (const auto& line : processedLines) {
+            sum += 10 * line.front();
+            sum += line.back();
         }
-        for (auto it = line.crbegin(); it != line.crend(); ++it) {
-            if (std::isdigit(*it)) {
-                sum += static_cast<int>(*it - '0');
-                break;
-            }
-        }
+        return sum;
     }
-    return sum;
-}
+
+};
 
 int main(int argc, char* argv[]) {
-    std::ifstream myfile("input.txt");
-    std::string line;
-    std::vector<std::string> myLines;
-    while (std::getline(myfile, line))
-    {
-        myLines.push_back(line);
-    }
 
-    int result = sumOuterNumbers(myLines);
+    Day1 day1{};
+
+    int result = day1.part1();
     std::cout << "Task 1: " << result << std::endl;
     
     return 1;
