0dd6739df2db15bb5fc4d354b83795335e8e838e
Oliver Sheldon <oliversheldon00@gmail.com>
2023-12-30 15:19:22 +0000

    Completed Python implementation for Day2 part 2 in python

--- day2/day2.py
+++ day2/day2.py
@@ -1,8 +1,6 @@
 from enum import StrEnum, auto
-from dataclasses import dataclass
 from collections import UserDict
-import pandas
-from IPython import embed
+from functools import reduce
 
 class Dice(StrEnum):
     RED = auto(),
@@ -10,7 +8,7 @@ class Dice(StrEnum):
     BLUE = auto()
 
 
-class DiceSet(UserDict):
+class DiceDict(UserDict):
 
     maximum_bag_contents = {
         Dice.RED : 12,
@@ -20,32 +18,39 @@ class DiceSet(UserDict):
 
     def __init__(self):
         super().__init__()
-        for dice in Dice:
-            self.update({dice:0})
+        for die in Dice:
+            self.update({die:0})
 
 
     def valid(self) -> bool:
-        if set(self.keys()) != set(dice for dice in Dice):
-            raise RuntimeError(f"Trying to check validity of invalid dice set: {self.items()}")
+        if set(self.keys()) != set(die for die in Dice):
+            raise RuntimeError(f"Trying to check validity of invalid die dice_set: {self.items()}")
 
-        for dice in Dice:
-            if self[dice] > DiceSet.maximum_bag_contents[dice]:
+        for die in Dice:
+            if self[die] > DiceDict.maximum_bag_contents[die]:
                 return False
         return True
+    
+    def update_max(self, dice_set) -> None:
+        for die in Dice:
+            if dice_set[die] > self[die]:
+                self[die] = dice_set[die]
 
     
 class Game:
     
     def __init__(self, id: int):
         self.id: int = id
-        self.sets: list[DiceSet] = []
+        self.dice_sets: list[DiceDict] = []
+        self.max_dice_set: DiceDict = DiceDict()
 
-    def add_set(self, set: DiceSet) -> None:
-        self.sets.append(set)
+    def add_set(self, dice_set: DiceDict) -> None:
+        self.max_dice_set.update_max(dice_set)
+        self.dice_sets.append(dice_set)
 
     def valid(self) -> bool:
-        for set in self.sets:
-            if not set.valid():
+        for dice_set in self.dice_sets:
+            if not dice_set.valid():
                 return False
         return True
 
@@ -67,30 +72,31 @@ class Day2:
         for line in self.input:
             game = Game(int(line.split()[1][:-1]))
             info = line[line.find(':')+2:]
-            sets = info.split(';')
-            for set in sets:
-                dice_set = DiceSet()
-                for num_and_colour in set.split(','):
+            dice_sets = info.split(';')
+            for dice_set in dice_sets:
+                set_of_dice = DiceDict()
+                for num_and_colour in dice_set.split(','):
                     num, colour = num_and_colour.split()
                     dice_colour = Dice(colour)
-                    dice_set[dice_colour] = int(num)
-                game.add_set(dice_set)
+                    set_of_dice[dice_colour] = int(num)
+                game.add_set(set_of_dice)
             games.append(game)
         return games
     
-    def part1(self):
-        valid_ids = self.get_valid_game_ids()
-        return sum(valid_ids)
+    def part1(self) -> int:
+        return sum(game.id for game in self.games if game.valid())
     
-    def get_valid_game_ids(self) -> list[int]:
-        ids = []
-        for game in self.games:
-            if game.valid():
-                ids.append(game.id)
-        return ids
+    def part2(self) -> int:
+        max_dice_required_per_game = (game.max_dice_set.values() for game in self.games)
+        power = 0
+        for max_dice_nums in max_dice_required_per_game:
+            power += reduce((lambda x, y: x * y), max_dice_nums)
+        return power
+
     
 
             
 if __name__ == "__main__":
     day2 = Day2()
     print(day2.part1())
+    print(day2.part2())
