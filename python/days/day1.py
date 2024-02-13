29aa1107a29674f1d2c10c9e7be2c714c9903c83
Oliver Sheldon <oliversheldon00@gmail.com>
2024-02-13 23:27:32 +0000

    Rewrote day1

--- python/days/day1.py
+++ python/days/day1.py
@@ -1,89 +1,103 @@
 import string
-import sys
 from overrides import override
-
+from enum import Enum, auto
 from aoc23_base import DayBase
 
+class ParseType(Enum):
+    DIGITS = auto()
+    DIGITS_AND_STRINGS = auto()
+
+class CalibrationLine:
+
+    STRING_TO_DIGIT: dict[str, int] = {
+        "one"   : 1,
+        "two"   : 2,
+        "three" : 3,
+        "four"  : 4,
+        "five"  : 5,
+        "six"   : 6,
+        "seven" : 7,
+        "eight" : 8,
+        "nine"  : 9,
+    }
+
+    def __init__(self, line: str):
+        self.line = line
+
+    def parse_edge_digits(self, parse_type: ParseType) -> tuple[int, int]:
+        match parse_type:
+            case ParseType.DIGITS:
+                return self._parse_edge_digits_only()
+            case ParseType.DIGITS_AND_STRINGS:
+                return self._parse_edge_digits_and_strings()
+            case _:
+                raise RuntimeError(f"ParseType {parse_type} is not recognised.")
+    
+    def _parse_edge_digits_only(self) -> tuple[int, int]:
+        left_edge_value, right_edge_value = -1, -1
+
+        for l in range(len(self.line)):
+            if self.line[l] in string.digits:
+                left_edge_value = int(self.line[l])
+                break
+
+        for r in range(len(self.line) - 1, -1, -1):
+            if self.line[r] in string.digits:
+                right_edge_value = int(self.line[r])
+                break
+
+        assert left_edge_value > 0 and right_edge_value > 0
+        return left_edge_value, right_edge_value
+    
+    def _parse_edge_digits_and_strings(self) -> tuple[int, int]:
+        left_edge_value, right_edge_value = -1, -1
+
+        for l in range(len(self.line)):
+            if self.line[l] in string.digits:
+                left_edge_value = int(self.line[l])
+            for key, val in CalibrationLine.STRING_TO_DIGIT.items():
+                if key in self.line[ : l]:
+                    left_edge_value = val
+            
+            if left_edge_value >= 0:
+                break
+
+        for r in range(len(self.line) - 1, -1, -1):
+            if self.line[r] in string.digits:
+                right_edge_value = int(self.line[r])
+            for key, val in CalibrationLine.STRING_TO_DIGIT.items():
+                if key in self.line[r : ]:
+                    right_edge_value = val
+            
+            if right_edge_value >= 0:
+                break
+
+        assert left_edge_value > 0 and right_edge_value > 0
+        return left_edge_value, right_edge_value
+    
+    def calibration_value(self, parse_type: ParseType = ParseType.DIGITS) -> int:
+        left_edge_value, right_edge_value = self.parse_edge_digits(parse_type)
+        return 10 * left_edge_value + right_edge_value
 
 class Day1(DayBase):
-
-    char_string_map = {"one"   : 1,
-                       "two"   : 2,
-                       "three" : 3,
-                       "four"  : 4,
-                       "five"  : 5,
-                       "six"   : 6,
-                       "seven" : 7,
-                       "eight" : 8,
-                       "nine"  : 9,
-                       }
-    min_char_string_length = min([len(key) for key in char_string_map])
-    max_char_string_length = max([len(key) for key in char_string_map])
-
-    inverse_char_string_map = {k[::-1] : v for k, v in char_string_map.items()}
-
+    
     def __init__(self):
-        self.input = self.get_input()
+        super().__init__()
+        self.calibration_lines = self.parse()
 
+    def parse(self) -> list[CalibrationLine]:
+        return [CalibrationLine(line) for line in self.input]
+
+    @override
     def part_1(self) -> int:
-        lines = Day1.preprocess_strings(self.input, Day1.string_preprocessing_part_1)
-        return Day1.handle_boundary_integers(lines)
-    
-    def part_2(self) -> int:
-        lines = Day1.preprocess_strings(self.input, Day1.string_preprocessing_part_2)
-        return Day1.handle_boundary_integers(lines)
+        return sum(calibration_line.calibration_value(parse_type=ParseType.DIGITS) for calibration_line in self.calibration_lines)
 
-    @staticmethod
-    def handle_boundary_integers(lines: list[list[int]]) -> int:
-        assert len(lines) >= 1 and all(all((type(item) == int) for item in line) for line in lines)
-        value = 0
-        for line in lines:
-            value += 10 * line[0] + line[-1]
-        return value
-    
-    @staticmethod
-    def preprocess_strings(lines: list[str], method) -> list[list[int]]:
-        new_lines = []
-        for line in lines:
-            new_lines.append(method(line))
-        return new_lines
-    
-    @staticmethod
-    def string_preprocessing_part_1(line: str) -> list[list[int]]:
-        new_line = []
-        for c in line:
-            if c.isdigit():
-                new_line.append(int(c))
-        return new_line
-    
-    @staticmethod
-    def string_preprocessing_part_2(line: str) -> list[int]:
-        new_line = []
-        l = 0
-        while l < len(line):
-            if line[l] in string.digits:
-                new_line.append(int(line[l]))
-            else:
-                answer = Day1.isStringRangeValid(line, l)
-                if answer:
-                    new_line.append(answer)
-            l += 1
-        return new_line
-    
-    @staticmethod
-    def isStringRangeValid(line, l):
-        r = l + Day1.min_char_string_length
-        while r - l <= Day1.max_char_string_length and r < len(line) + 1:
-            if line[l:r] in Day1.char_string_map:
-                return Day1.char_string_map[line[l:r]]
-            r += 1
-        return 0
+    @override
+    def part_2(self) -> int:
+        return sum(calibration_line.calibration_value(parse_type=ParseType.DIGITS_AND_STRINGS) for calibration_line in self.calibration_lines)
 
 if __name__ == "__main__":
-
     day1 = Day1()
-
-    print("Python Solution:")
-    print(f"\tPart 1: {day1.part_1()}")
-    print(f"\tPart 2: {day1.part_2()}")
+    print(day1.part_1())
+    print(day1.part_2())
     
