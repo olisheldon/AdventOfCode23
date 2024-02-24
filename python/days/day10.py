c5e02da9a799f871434e920d3402d257443f4d32
Oliver Sheldon <oliversheldon00@gmail.com>
2024-02-24 20:14:18 +0000

    Progress on day10, not working for part 2

--- python/days/day10.py
+++ python/days/day10.py
@@ -14,15 +14,23 @@ class Coord:
 class Segment(Enum):
     INSIDE = auto()
     OUTSIDE = auto()
-    NEITHER = auto()
+    LOOP = auto()
+
+    UNDETERMINED = auto()
+
+    @classmethod
+    def crossings_to_segment(cls, crossings: int) -> 'Segment':
+        if crossings % 2 == 0:
+            return cls.OUTSIDE
+        return cls.INSIDE
 
 class Move(Enum):
-    NORTH = auto() 
-    EAST = auto() 
-    SOUTH = auto() 
+    NORTH = auto()
+    EAST = auto()
+    SOUTH = auto()
     WEST = auto()
 
-    INVALID = auto() 
+    INVALID = auto()
 
     @classmethod
     def move_to_coord_offset(cls, move: 'Move') -> Coord:
@@ -39,14 +47,14 @@ class Move(Enum):
                 return Coord(0, 0)
 
 class TileType(Enum):
-    VERTICAL = auto() 
-    HORIZONTAL = auto() 
-    BENDNE = auto() 
-    BENDNW = auto() 
-    BENDSW = auto() 
-    BENDSE = auto() 
-    GROUND = auto() 
-    START = auto() 
+    VERTICAL = auto()
+    HORIZONTAL = auto()
+    BENDNE = auto()
+    BENDNW = auto()
+    BENDSW = auto()
+    BENDSE = auto()
+    GROUND = auto()
+    START = auto()
 
 
     @classmethod
@@ -72,14 +80,35 @@ class TileType(Enum):
                 raise RuntimeError(f"Tile type {s} is not recognised.")
 
 
+    @classmethod
+    def tile_type_from_moves(cls, moves: list[Move]) -> 'TileType':
+        set_moves: set[Move] = set(moves)
+
+        if set_moves == set([Move.NORTH, Move.SOUTH]):
+            return cls.VERTICAL
+        if set_moves == set([Move.EAST, Move.WEST]):
+            return cls.HORIZONTAL
+        if set_moves == set([Move.NORTH, Move.EAST]):
+            return cls.BENDNE
+        if set_moves == set([Move.NORTH, Move.WEST]):
+            return cls.BENDNW
+        if set_moves == set([Move.SOUTH, Move.WEST]):
+            return cls.BENDSW
+        if set_moves == set([Move.SOUTH, Move.EAST]):
+            return cls.BENDSE
+        
+        raise RuntimeError(f"Can not handle moves={moves}")
+
+
 class Tile:
 
-    def __init__(self, tile_char: str):
+    def __init__(self, tile_char: str, segment: Segment = Segment.UNDETERMINED):
         self.tile_type: TileType = TileType.from_str(tile_char)
-        self.segment: Segment = Segment.NEITHER
+        self.tile_char: str = tile_char
+        self.segment: Segment = segment
     
     def __repr__(self) -> str:
-        return self.tile_type.name
+        return self.tile_char
             
     def move(self, move_in: Move) -> Move:
         match self.tile_type:
@@ -134,60 +163,98 @@ class Tile:
                     case _:
                         return Move.INVALID
             case TileType.GROUND:
-                raise RuntimeError(f"Tile type {move_in} should not be called with {self.__name__}.")
+                raise RuntimeError(f"Tile type {move_in} should not be called with {self.__class__.__name__}.")
             case _:
                 raise RuntimeError(f"Tile type {move_in} not recognised.")
-            
-    def declare_segment(self, segment: Segment):
-        if self.tile_type == TileType.GROUND:
-            self.segment = segment
 
 
 class Maze:
 
     def __init__(self, tiles: list[str]):
-        self.tiles: dict[Coord, Tile] = {}
+        self.tiles: list[list[Tile]] = []
         self.start: Coord = Coord(0, 0)
         for i in range(len(tiles)):
+            self.tiles.append([])
             for j in range(len(tiles[0])):
-                coord = Coord(i, j)
-                self.tiles[coord] = Tile(tiles[i][j])
-                if self.tiles[coord].tile_type == TileType.START:
-                    self.start = coord
+                self.tiles[i].append(Tile(tiles[i][j]))
+                if self.tiles[i][j].tile_type == TileType.START:
+                    self.start = Coord(i, j)
+
+    def __repr__(self, segment: Segment = Segment.LOOP) -> str:
+        s = ""
+        for row in self.tiles:
+            for elem in row:
+                if elem.segment is segment:
+                    s += '#'
+                else:
+                    s += str(elem)
+            s += "\n"
+        return s
+    
+    def show_segment(self, segment: Segment = Segment.LOOP) -> str:
+        return self.__repr__(segment)
 
     def get_beginning_moves(self) -> list[Move]:
-        return list(move for move in Move if self.tiles[self.start + Move.move_to_coord_offset(move)].move(move) != Move.INVALID)
+        return list(move for move in Move if self.tiles[(self.start + Move.move_to_coord_offset(move)).i][(self.start + Move.move_to_coord_offset(move)).j].move(move) != Move.INVALID)
     
     def traverse_pipes(self) -> int:
         beginning_moves: list[Move] = self.get_beginning_moves()
-        move = beginning_moves[0]
+        start_tile_type: TileType = TileType.tile_type_from_moves(beginning_moves)
+        self.tiles[self.start.i][self.start.j].tile_type = start_tile_type
+        self.tiles[self.start.i][self.start.j].segment = Segment.LOOP
+        move = beginning_moves[0] # pick random valid direction
         move_count = 1
         coord = self.start + Move.move_to_coord_offset(move)
-        while self.tiles[coord].tile_type != TileType.START:
-            move = self.tiles[coord].move(move)
+
+        while coord != self.start:
+            move = self.tiles[coord.i][coord.j].move(move)
             coord += Move.move_to_coord_offset(move)
             move_count += 1
+
+            self.tiles[coord.i][coord.j].segment = Segment.LOOP
         return move_count
     
     def furthest_point(self) -> int:
         move_count = self.traverse_pipes()
         return move_count // 2 + 1 if move_count % 2 else move_count // 2
     
-    def spread_segment(self):
-        coord_offsets = (Coord(-1, -1), Coord(1, -1), Coord(-1, 1), Coord(1, 1))
-        for tile_coord, tile in self.tiles.items():
-            if tile.tile_type == TileType.GROUND and tile.segment:
-                for coord_offset in coord_offsets:
-                    coord = tile_coord + coord_offset
-                    if coord in self.tiles:
-                        self.tiles[coord].segment = tile.segment
-    
-    def count_ground_inside(self) -> int:
-        count = 0
-        for tile in self.tiles.values():
-            if tile.segment == Segment.INSIDE:
-                count += 1
-        return count
+    def partition(self) -> None:
+        height: int = len(self.tiles)
+        width: int = len(self.tiles[0])
+
+        class Direction(Enum):
+            NS = auto()
+            WE = auto()
+            NEITHER = auto()
+        
+        for i in range(height):
+            inside: bool = False
+            prev_bend: TileType = TileType.GROUND
+            for j in range(width):
+                coord = Coord(i, j)
+                tile = self.tiles[coord.i][coord.j]
+                
+                if tile.segment is Segment.LOOP:
+                    if tile.tile_type is TileType.VERTICAL:
+                        inside = not inside
+                    elif tile.tile_type is TileType.HORIZONTAL:
+                        pass
+                    elif tile.tile_type in (TileType.BENDNE, TileType.BENDSE):
+                        prev_bend = tile.tile_type
+                    elif tile.tile_type is TileType.BENDNW:
+                        if prev_bend is TileType.BENDSE:
+                            inside = not inside
+                    elif tile.tile_type is TileType.BENDSW:
+                        if prev_bend is TileType.BENDNE:
+                            inside = not inside
+                    else:
+                        raise RuntimeError()
+                    
+                if tile.segment is not Segment.LOOP:
+                    tile.segment = Segment.INSIDE if inside else Segment.OUTSIDE
+
+    def count_segments(self, segment: Segment) -> int:
+        return sum(tile.segment is segment for row in self.tiles for tile in row)
 
     
 
@@ -208,8 +275,13 @@ class Day10(DayBase):
     def part_2(self) -> int:
         maze = Maze(self.input)
         maze.traverse_pipes()
-        maze.spread_segment()
-        return maze.count_ground_inside()
+        maze.partition()
+        print([(segment, maze.count_segments(segment)) for segment in Segment])
+        counts = maze.count_segments(Segment.INSIDE)
+
+        print(maze.show_segment(Segment.INSIDE))
+        return counts
+
 
 
 if __name__ == "__main__":
