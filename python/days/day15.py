767a73458b23c541950d353e079e7ed08edaf727
Oliver Sheldon <oliversheldon00@gmail.com>
2024-03-01 19:04:52 +0000

    Simplified solution to day 15 in Python

--- python/days/day15.py
+++ python/days/day15.py
@@ -1,42 +1,50 @@
 from overrides import override
-from collections import deque
 from aoc23_base import DayBase
 from enum import Enum, auto
-
+import abc
 
 class Operation(Enum):
     EQUALS = auto()
     MINUS = auto()
 
     @classmethod
-    def from_str(cls, c: str) -> 'Operation':
+    def from_str(cls, s: str) -> 'Operation':
+        if '=' in s and '-' not in s:
+            return cls.EQUALS
+        if '-' in s and '=' not in s:
+            return cls.MINUS
+        raise RuntimeError(f"String {s} does not contain only equals or minus.")
+
+    @classmethod
+    def from_char(cls, c: str) -> 'Operation':
         match c:
             case '=':
                 return cls.EQUALS
             case '-':
                 return cls.MINUS
             case _:
-                raise RuntimeError(f"Operation {c} is not recognised.")
+                raise RuntimeError(f"Char {c} is not recognised.")
                        
     @classmethod
-    def from_platform_object(cls, s: 'Operation') -> str:
+    def from_operation(cls, s: 'Operation') -> str:
         match s:
             case cls.EQUALS:
                 return '-'
             case cls.MINUS:
-                return '#'
+                return '='
             case _:
                 raise RuntimeError(f"Operation {s} is not recognised.")
     
-            
 class Hasher:
 
     @staticmethod
-    def hash(s: str) -> int:
-        # Determine the ASCII code for the current character of the string.
-        # Increase the current focal_length by the ASCII code you just determined.
-        # Set the current focal_length to itself multiplied by 17.
-        # Set the current focal_length to the remainder of dividing itself by 256.
+    def hash_value(s: str) -> int:
+        '''
+        Determine the ASCII code for the current character of the string.
+        Increase the current focal_length by the ASCII code you just determined.
+        Set the current focal_length to itself multiplied by 17.
+        Set the current focal_length to the remainder of dividing itself by 256.
+        '''
         current_value = 0
         for c in s:
             ascii_value = ord(c)
@@ -45,74 +53,94 @@ class Hasher:
             current_value %= 256
         return current_value
 
+class InitSequenceElementBase(abc.ABC):
 
-class InitSequenceElement:
-
-    def __init__(self, s: str):
-        hash_value = None
-        operation = None
-        label = None
-        focal_length = None
-        for c in ['-', '=']:
-            if c in s:
-                label: str = s.split(c)[0]
-                hash_value: int = Hasher.hash(s.split(c)[0])
-                operation: Operation = Operation.from_str(c)
-                if operation is Operation.EQUALS:
-                    focal_length = int(s.split(c)[-1])
-        self.hash_value: int = hash_value
-        self.label = label
+    def __init__(self, operation: Operation, name: str):
         self.operation: Operation = operation
-        self.focal_length: None | int = focal_length or None
+        self.name: str = name
+        self.hash_value: int = Hasher.hash_value(name)
     
-    def __eq__(self, other: 'InitSequenceElement'):
-        return self.label == other.label
+    @classmethod
+    def create_init_sequence_element(cls, s: str) -> 'InitSequenceElementBase':
+        if '=' in s:
+            name, val = s.split('=')
+            return EqualInitSequenceElement(name, int(val))
+        elif '-' in s:
+            name = s[:-1]
+            return MinusInitSequenceElement(name)
+        raise RuntimeError(f"SequenceElement {s} is invalid.")
+            
+    @abc.abstractmethod
+    def operate_on_boxes(self, boxes: 'Boxes') -> None:
+        pass
+
+class EqualInitSequenceElement(InitSequenceElementBase):
+
+    def __init__(self, name: str, value: int):
+        super().__init__(Operation.EQUALS, name)
+        self.value: int = value
+
+    @override
+    def operate_on_boxes(self, boxes: 'Boxes') -> None:
+        boxes.add(self.name, self.hash_value, self.value)
+
+
+class MinusInitSequenceElement(InitSequenceElementBase):
+
+    def __init__(self, name: str):
+        super().__init__(Operation.MINUS, name)
+
+    @override
+    def operate_on_boxes(self, boxes: 'Boxes') -> None:
+        boxes.remove(self.name, self.hash_value)
+
+class Boxes:
+
+    def __init__(self):
+        self.boxes: list[list[str]] = [[] for _ in range(256)]
+        self.focal_length_map: dict[str, int] = {}
+
+    def process_sequence(self, sequence: list[InitSequenceElementBase]) -> None:
+        for element in sequence:
+            element.operate_on_boxes(self)
+    
+    def remove(self, name: str, hash_value: int) -> None:
+        if name in self.boxes[hash_value]:
+            self.boxes[hash_value].remove(name)
+    
+    def add(self, name: str, hash_value: int, value: int) -> None:
+        if name not in self.boxes[hash_value]:
+            self.boxes[hash_value].append(name)
+        self.focal_length_map[name] = value
+    
+    @property
+    def focusing_power(self) -> int:
+        focusing_power = 0
+        for box_index, box in enumerate(self.boxes):
+            for lens_index, name in enumerate(box):
+                focusing_power += (box_index + 1) * (lens_index + 1) * self.focal_length_map[name]
+        return focusing_power
 
 
 class Day15(DayBase):
     
     def __init__(self):
         super().__init__()
-        self.boxes: list[deque[InitSequenceElement]] = [deque() for _ in range(256)]
+        
 
     def parse(self) -> list[str]:
         return self.input[0].split(',')
 
     @override
     def part_1(self) -> int:
-        return sum(Hasher.hash(s) for s in self.parse())
-        
+        return sum(map(Hasher.hash_value, self.parse()))
 
     @override
     def part_2(self) -> int:
-        sequence_elements = [InitSequenceElement(s) for s in self.parse()]
-        for sequence_element in sequence_elements:
-            box = self.boxes[sequence_element.hash_value]
-            match sequence_element.operation:
-                case Operation.MINUS:
-                    for i, already_present_sequence_element in enumerate(box):
-                        if sequence_element == already_present_sequence_element:
-                            del box[i]
-                            break
-                case Operation.EQUALS:
-                    assigned = False
-                    for i, already_present_sequence_element in enumerate(box):
-                        if sequence_element == already_present_sequence_element:
-                            box[i] = sequence_element
-                            assigned = True
-                            break
-                    if not assigned:
-                        box.append(sequence_element)
-                case _:
-                    raise RuntimeError(f"operation {sequence_element.operation} not recognised.")
-
-        score = 0
-        for box_number, box in enumerate(self.boxes):
-            for sequence_number, sequence_element in enumerate(box):
-                score += (box_number + 1) * (sequence_number + 1) * (sequence_element.focal_length)
-        return score
-
-
+        sequence_elements: list[InitSequenceElementBase] = [InitSequenceElementBase.create_init_sequence_element(s) for s in self.parse()]
+        boxes = Boxes()
+        boxes.process_sequence(sequence_elements)
+        return boxes.focusing_power
 
 
 
