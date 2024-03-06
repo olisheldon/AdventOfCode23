c06ad6a8df094e6b043e83ce0f365871d74a4c8e
Oliver Sheldon <oliversheldon00@gmail.com>
2024-03-06 00:28:30 +0000

    Refactored day 1

--- python/days/day1.py
+++ python/days/day1.py
@@ -5,79 +5,53 @@ from aoc23_base import DayBase
 
 class ParseType(Enum):
     DIGITS = auto()
+    STRINGS = auto()
     DIGITS_AND_STRINGS = auto()
+
+    @classmethod
+    def get_string_interpret(cls, parse_type: 'ParseType') -> dict[str, int]:
+        match parse_type:
+            case cls.DIGITS:
+                return {str(d) : int(d) for d in string.digits}
+            case cls.STRINGS:
+                return {
+                    "one"   : 1,
+                    "two"   : 2,
+                    "three" : 3,
+                    "four"  : 4,
+                    "five"  : 5,
+                    "six"   : 6,
+                    "seven" : 7,
+                    "eight" : 8,
+                    "nine"  : 9,
+                }
+            case cls.DIGITS_AND_STRINGS:
+                return cls.get_string_interpret(cls.DIGITS) | cls.get_string_interpret(cls.STRINGS)
+            case _:
+                raise RuntimeError(f"ParseType {parse_type} is not recognised.")
 
 class CalibrationLine:
 
-    STRING_TO_DIGIT: dict[str, int] = {
-        "one"   : 1,
-        "two"   : 2,
-        "three" : 3,
-        "four"  : 4,
-        "five"  : 5,
-        "six"   : 6,
-        "seven" : 7,
-        "eight" : 8,
-        "nine"  : 9,
-    }
-
     def __init__(self, line: str):
         self.line = line
-
-    def parse_edge_digits(self, parse_type: ParseType) -> tuple[int, int]:
-        match parse_type:
-            case ParseType.DIGITS:
-                return self._parse_edge_digits_only()
-            case ParseType.DIGITS_AND_STRINGS:
-                return self._parse_edge_digits_and_strings()
-            case _:
-                raise RuntimeError(f"ParseType {parse_type} is not recognised.")
-    
-    def _parse_edge_digits_only(self) -> tuple[int, int]:
-        left_edge_value, right_edge_value = -1, -1
-
-        for l in range(len(self.line)):
-            if self.line[l] in string.digits:
-                left_edge_value = int(self.line[l])
-                break
-
-        for r in range(len(self.line) - 1, -1, -1):
-            if self.line[r] in string.digits:
-                right_edge_value = int(self.line[r])
-                break
-
-        assert left_edge_value > 0 and right_edge_value > 0
-        return left_edge_value, right_edge_value
-    
-    def _parse_edge_digits_and_strings(self) -> tuple[int, int]:
-        left_edge_value, right_edge_value = -1, -1
-
-        for l in range(len(self.line)):
-            if self.line[l] in string.digits:
-                left_edge_value = int(self.line[l])
-            for key, val in CalibrationLine.STRING_TO_DIGIT.items():
-                if key in self.line[ : l]:
-                    left_edge_value = val
-            
-            if left_edge_value >= 0:
-                break
-
-        for r in range(len(self.line) - 1, -1, -1):
-            if self.line[r] in string.digits:
-                right_edge_value = int(self.line[r])
-            for key, val in CalibrationLine.STRING_TO_DIGIT.items():
-                if key in self.line[r : ]:
-                    right_edge_value = val
-            
-            if right_edge_value >= 0:
-                break
-
-        assert left_edge_value > 0 and right_edge_value > 0
-        return left_edge_value, right_edge_value
     
     def calibration_value(self, parse_type: ParseType = ParseType.DIGITS) -> int:
-        left_edge_value, right_edge_value = self.parse_edge_digits(parse_type)
+        left_edge_value, right_edge_value = self._parse_boundary(parse_type)
         return 10 * left_edge_value + right_edge_value
+
+    def _parse_boundary(self, parse_type) -> tuple[int, int]:
+        string_interpret: dict[str, int] = ParseType.get_string_interpret(parse_type)
+        left_edge_value, right_edge_value = self._parse(self.line, string_interpret), self._parse(self.line[::-1], {k[::-1] : v for k, v in string_interpret.items()})
+        assert left_edge_value > -1 and right_edge_value > -1
+        return left_edge_value, right_edge_value
+
+    @staticmethod
+    def _parse(line: str, string_interpret: dict[str, int]) -> int:
+        for l in range(1, len(line) + 1):
+            for key, val in string_interpret.items():
+                if key in line[ : l]:
+                    return val
+        return -1
 
 class Day1(DayBase):
     
