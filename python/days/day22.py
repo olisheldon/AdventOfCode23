30eb8839d136e7af0c212f9ff88578f1d7dd8288
Oliver Sheldon <oliversheldon00@gmail.com>
2024-03-15 17:59:27 +0000

    Completed day 22 part 1

--- python/days/day22.py
+++ python/days/day22.py
@@ -2,80 +2,82 @@ from overrides import override
 from aoc23_base import DayBase
 from enum import Enum, Flag, auto
 from dataclasses import dataclass
+from typing import Sequence
 
 
-@dataclass(frozen=True)
+@dataclass()
 class Coord:
     x: int
     y: int
     z: int
 
     @classmethod
-    def get_extent(cls, coord1: 'Coord', coord2: 'Coord') -> list['Coord']:
-        if coord1.x - coord2.x:
-            assert coord1.y == coord2.y and coord1.z == coord2.z
-            return [Coord(x, coord1.y, coord1.z) for x in range(max(coord1.x - coord2.x, coord2.x - coord1.x) + 1)]
-        if coord1.y - coord2.y:
-            assert coord1.x == coord2.x and coord1.z == coord2.z
-            return [Coord(coord1.y, y, coord1.z) for y in range(max(coord1.y - coord2.y, coord2.y - coord1.y) + 1)]
-        if coord1.z - coord2.z:
-            assert coord1.x == coord2.x and coord1.y == coord2.y
-            return [Coord(coord1.x, coord1.y, z) for z in range(max(coord1.z - coord2.z, coord2.z - coord1.z) + 1)]
-        raise RuntimeError(f"{coord1} and {coord2} have no extent.")
-
-    @classmethod
-    def from_str(cls, brick_str: str) -> 'Coord':
+    def from_csv(cls, brick_str: str) -> 'Coord':
         x, y, z = brick_str.split(',')
         return Coord(int(x), int(y), int(z))
-
-    @classmethod
-    def coord_below(cls, coord: 'Coord') -> 'Coord':
-        return Coord(coord.x, coord.y, coord.z - 1)
 
 
 class Brick:
 
     def __init__(self, brick_str: str):
         first_coord, second_coord = brick_str.split('~')
-        self.initial_coords: list[Coord] = Coord.get_extent(
-            Coord.from_str(first_coord), Coord.from_str(second_coord))
+        self.coord1: Coord = Coord.from_csv(first_coord)
+        self.coord2: Coord = Coord.from_csv(second_coord)
+
+    def __repr__(self) -> str:
+        return f"Brick(coord1={self.coord1}, coord2={self.coord2})"
+
+    def overlaps(self, other: 'Brick') -> bool:
+        return max(self.coord1.x, other.coord1.x) <= min(self.coord2.x, other.coord2.x) and max(self.coord1.y, other.coord1.y) <= min(self.coord2.y, other.coord2.y)
 
 
 class BrickContainer:
 
     def __init__(self, bricks: list[str]):
-        self.bricks: list[Brick] = [Brick(line) for line in bricks]
-        self.occupied_coords: set[Coord] = set()
-        self.brick_lookup: dict[tuple[Coord, ...], Brick] = {}
-        for brick in self.bricks:
-            self.brick_lookup[tuple(brick.initial_coords)] = brick
-            self.occupied_coords.union(brick.initial_coords)
+        self.snapshot_bricks: tuple[Brick, ...] = tuple(
+            Brick(line) for line in bricks)
 
-    def update_until_settled(self) -> None:
-        old_occupied_coords = self.occupied_coords.copy()
-        self._update_brick_positions()
-        while self.occupied_coords != old_occupied_coords:
-            old_occupied_coords = self.occupied_coords.copy()
-            self._update_brick_positions()
+    @staticmethod
+    def _fall(bricks: list[Brick]) -> list[Brick]:
+        bricks.sort(key=lambda brick: brick.coord1.z)
+        for i, brick in enumerate(bricks):
+            max_z = 1
+            for other in bricks[:i]:
+                if brick.overlaps(other):
+                    max_z = max(max_z, other.coord2.z + 1)
+            brick.coord2.z -= brick.coord1.z - max_z
+            brick.coord1.z = max_z
 
-    def _update_brick_positions(self):
-        for brick_coords, brick in self.brick_lookup.items():
-            if self._can_brick_move(brick_coords):
-                coords_below = tuple(Coord.coord_below(brick_coord)
-                                     for brick_coord in brick_coords)
-                self.brick_lookup[coords_below] = brick
-                self.brick_lookup.pop(brick_coords)
-                self.occupied_coords = (
-                    self.occupied_coords - set(brick_coords)).union(set(coords_below))
-                break
+        return bricks
 
-    def _can_brick_move(self, brick_coords: tuple[Coord, ...]) -> bool:
-        occupied_coords_except_current_brick: set[Coord] = self.occupied_coords - set(
-            brick_coords)
-        for new_coord in map(Coord.coord_below, brick_coords):
-            if new_coord.z <= 0 or new_coord in occupied_coords_except_current_brick:
-                return False
-        return True
+    @staticmethod
+    def _bidirectional_supports(bricks: Sequence[Brick]) -> tuple[dict[int, set[int]], dict[int, set[int]]]:
+
+        bricks_supporting = {i: set() for i in range(len(bricks))}
+        supporting_bricks = {i: set() for i in range(len(bricks))}
+
+        for j, above in enumerate(bricks):
+            for i, below in enumerate(bricks[:j]):
+                if below.overlaps(above) and above.coord1.z == below.coord2.z + 1:
+                    bricks_supporting[i].add(j)
+                    supporting_bricks[j].add(i)
+
+        return bricks_supporting, supporting_bricks
+
+    def safe_disintegration_count(self) -> int:
+
+        bricks = self._fall(list(self.snapshot_bricks))
+
+        bricks_supporting, supports_bricks = self._bidirectional_supports(
+            bricks)
+
+        total = 0
+
+        for i in range(len(bricks)):
+            if all(len(supports_bricks[j]) >= 2 for j in bricks_supporting[i]):
+                total += 1
+
+        return total
 
 
 class Day22(DayBase):
@@ -84,12 +86,9 @@ class Day22(DayBase):
         super().__init__()
         self.brick_container = BrickContainer(self.input)
 
-    def parse(self) -> list[str]:
-        pass
-
     @override
     def part_1(self) -> int:
-        self.brick_container.update_until_settled()
+        return self.brick_container.safe_disintegration_count()
 
     @override
     def part_2(self) -> int:
